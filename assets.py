18bb1473d02e1cedadd1928beb3ce7e12cea8e0f
dalek <abigailmarydelpiero@gmail.com>
2026-05-14 21:18:49 -0400

    first code commit

---
+++ assets.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import pygame
+
+BASE_DIR = Path(__file__).resolve().parent
+"""
+PLAYER = pygame.image.load(
+    BASE_DIR / "assets/images/player.png"
+).convert_alpha()
+
+JUMP = pygame.mixer.Sound(
+    BASE_DIR / "assets/sounds/jump.wav"
+)
+"""
--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Shark Kitty")
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Space Shooter")
 
 running = True
 
+# plain surface
+surf = pygame.Surface((100,200))
+surf.fill('orange')
+x = 100
+
+#importing an image
+player_surf = pygame.image.load('/home/dalek/shark-kitty/assets/images/DurrrSpaceShip.png')
 while running:
+    #event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill((30, 30, 30))
+    #draw the game
+    screen.fill('#282828')
+    x += 0.1
+    screen.blit(player_surf,(x,150))
+    #actual draw
     pygame.display.flip()
 
 pygame.quit()
