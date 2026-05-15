4a67504bb08443b0263003d19cfe03f03ef807f9
dalek <abigailmarydelpiero@gmail.com>
2026-05-14 20:13:02 -0400

    inital commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Shark Kitty")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((30, 30, 30))
+    pygame.display.flip()
+
+pygame.quit()
