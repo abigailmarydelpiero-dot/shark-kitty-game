7e049496e6831af3d73bcd233063bdebd1a63c3d
dalek <abigailmarydelpiero@gmail.com>
2026-05-18 21:32:19 -0400

    name and class reconstruction

--- main.py
+++ main.py
@@ -15,13 +15,27 @@ class Meat(pygame.sprite.Sprite):
             happyness += 50
 
 class Sharkitty(pygame.sprite.Sprite):
-    def __init__(self, name):
+    def __init__(self):
         super().__init__()
         self.image = pygame.image.load("shark.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.topleft = (175, 175) 
         self.rect_start_pos = self.rect.topleft
         self.sick = False
+        self.name = "no name :("
+    def enter_name(self, event):
+        global user_text
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                user_text = user_text[:-1]
+            elif event.key == pygame.K_RETURN:
+                print(f"Submitted Text: {user_text}")
+                self.name = user_text
+                user_text = ""
+            else:
+                user_text += event.unicode
+
 
 
 pygame.init()
@@ -39,58 +53,53 @@ black = '#282828'
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 #pygame.event.set_grab(True)
-def render_text(text, value):
-    text_display = my_font.render(f"{text}" + ":" + f"{value}", True, (255, 255, 255))
-    screen.blit(text_display, (100, 100)) 
+
+def render_text(text, value, color, x, y):
+    text_string = f"{text}" + f"{value}" 
+    text_display = my_font.render(text_string, True, color)
+    screen.blit(text_display, (x, y)) 
+
+
 gold = 0
 age = 0
 happyness = 400
-
+user_text = ""
 get_gold = True
-'''
-sick = pygame.image.load("good_meat.png").convert_alpha()
-sick_rect = sick.get_rect()
-sick_rect.topleft = (210, 150) 
 
-shark = pygame.image.load("shark.png").convert_alpha()
-shark_rect = shark.get_rect()
-shark_rect.topleft = (175, 175) 
-shark_rect_start_pos = shark_rect.topleft
-shark_sick = False
-'''
 #meat = Meat(random.randint(100, 200), 100)
 #all_sprites.add(meat)
-shark = Sharkitty("Bob")
-all_sprites.add(Sharkitty(shark))
+shark = Sharkitty()
+all_sprites.add(shark)
 #event loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     #print (pygame.mouse.get_focused())
-    
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if event.button == 1:
-            shark.rect.y -=200 * dt
-            if get_gold:
-                gold += 1
-                meat = Meat(random.randint(50, 300), -20)
-                all_sprites.add(meat)
-                happyness -= 10
-                get_gold = False
-    if event.type == pygame.MOUSEBUTTONUP:
-        if event.button == 1:
-            shark.rect.y =shark.rect_start_pos[1]
-            get_gold = True
+        shark.enter_name(event)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                shark.rect.y -=200 * dt
+                if get_gold:
+                    gold += 1
+                    meat = Meat(random.randint(50, 300), -20)
+                    all_sprites.add(meat)
+                    happyness -= 10
+                    get_gold = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                shark.rect.y =shark.rect_start_pos[1]
+                get_gold = True
    
     if happyness <= 0:
         sys.exit()
     
     #gold_text = my_font.render(f"Gold - {gold}", True, (255, 255, 255))
     #happyness_text = my_font.render(f"Happiness - {happyness}", True, (222, 215, 11))
-    render_text('Gold', gold)
-    if (random.randint(0,2) == 1):
+    
+
+    if (random.randint(0,100) == 1):
             shark_sick = True
-            print("IH")
+
             #screen.blit(sick, sick_rect)
     all_sprites.update() 
     #Sharkitty.update()
@@ -99,7 +108,10 @@ while True:
     #screen.blit(Sharkitty.image, Sharkitty.rect)
     #screen.blit(gold_text, (100, 100)) 
     #screen.blit(happyness_text, (100, 200)) 
-
+    render_text('Gold: ', gold, (255, 255, 0), 100, 100)
+    render_text('Happiness: ', happyness, (255, 255, 255), 100 , 150)
+    render_text('Name Input: ', user_text, (0, 255, 255), 100, 200)
+    render_text('', shark.name, (255, 255, 255), 600, 70)
     all_sprites.draw(screen)
     pygame.display.flip()
     dt = clock.tick(60) / 1000
